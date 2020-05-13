7d312400448016a74143bd0142fa070fee65b0db
Duncan Eastoe <duncan.eastoe@att.com>
2020-05-13 16:14:49 +0100

    MIBMisc: document key data structures
    Add comments to describe the two key data structures defined in
    MIBMisc.pm: $mib_tree, and $sorted_mib_tree.

--- lib/Vyatta/MIBMisc.pm
+++ lib/Vyatta/MIBMisc.pm
@@ -17,7 +17,45 @@ use base qw( Exporter );
 our @EXPORT =
   qw(create_mib_tree clear_mib_trees sort_mib_keys add_mib_entry get_oid get_next_oid set_oid);
 
+# Sub-agent MIB data tree
+#
+# Implemented as a hash of registered OIDs each mapping to a hash of child OIDs,
+# representing instantiated objects, mapping to an array of (<type>, <value>).
+#
+# eg. assuming the sub-agent registered to handle OID trees .1.2 and .1.3
+#
+#   {
+#      ".1.2" => {
+#                   ".1.2.1.2" => (ASN_OCTET_STR, "foo"),
+#                   ".1.2.1.1" => (ASN_INTEGER, 1),
+#                },
+#      ".1.3" => {
+#                   ".1.3.1.10" => (ASN_OCTET_STR, "bar"),
+#                   ".1.3.1.11" => (ASN_INTEGER, 20),
+#                },
+#   }
+#
+# The child hash for each registered OID is initialised by a call to
+# create_mib_tree().
+#
+# An instantiated object is inserted into the data tree by a call to
+# add_mib_entry().
 my $mib_tree        = {};
+
+# Sub-agent sorted MIB OIDs
+#
+# Implemented as a hash of registered OIDs each mapping to a sorted array
+# of their child OIDs, used to support GETNEXT operations. This is required
+# since the MIB data tree hash is inherently unsorted.
+#
+# eg. assuming the sub-agent registered to handle OID trees .1.2 and .1.3
+#
+#   {
+#      ".1.2" => ( ".1.2.1.1", ".1.2.1.2" ),
+#      ".1.3" => ( ".1.3.1.10", ".1.3.1.11" ),
+#   }
+#
+# Each array is only populated by a call to sort_mib_keys().
 my $sorted_mib_tree = {};
 
 sub by_oid ($$) {
